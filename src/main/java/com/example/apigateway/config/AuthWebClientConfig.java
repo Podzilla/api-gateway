bda181b4daedcbae702b0525e1a4de0a749d9a97
omar mohammed <omarb2515@gmail.com>
2025-05-10 12:36:02 +0300

    add web client bean

---
+++ src/main/java/com/example/apigateway/config/AuthWebClientConfig.java
@@ -0,0 +1,16 @@
+package com.example.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class AuthWebClientConfig {
+
+    @Bean
+    public WebClient authWebClient() {
+        return WebClient.builder()
+                .baseUrl("http://localhost:8080")
+                .build();
+    }
+}
